1b21d6de9f236284f01ea9dc535a74e58e77bb27
KusAndiPriyono <andipriyono94@gmail.com>
2023-11-17 22:52:32 +0700

    Initial commit

---
+++ app/src/main/java/com/bangkit/crabify/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.bangkit.crabify
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+        }
+        setContentView(R.layout.activity_main)
+
+    }
+}
