49c174ccbd5d1945b5b6a11ef30bd725a2ac3aee
KusAndiPriyono <andipriyono94@gmail.com>
2023-11-17 23:16:46 +0700

    Set Animation Splash Screen

--- app/src/main/java/com/bangkit/crabify/MainActivity.kt
+++ app/src/main/java/com/bangkit/crabify/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.bangkit.crabify
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,29 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen().apply {
             setKeepOnScreenCondition {
                 !viewModel.isReady.value
+            }
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.8f,
+                    0.0f
+                )
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.8f,
+                    0.0f
+                )
+
+                zoomX.interpolator = OvershootInterpolator()
+                zoomY.interpolator = OvershootInterpolator()
+                zoomX.duration = 500
+                zoomY.duration = 500
+                zoomX.doOnEnd { screen.remove() }
+                zoomX.start()
+                zoomY.doOnEnd { screen.remove() }
+                zoomY.start()
             }
         }
         setContentView(R.layout.activity_main)
