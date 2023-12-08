816a24a9bf0a5914dff0bad71e0e170109d7b58f
KusAndiPriyono <andipriyono94@gmail.com>
2023-12-08 21:15:08 +0700

    UI Authentication

--- app/src/main/java/com/bangkit/crabify/MainViewModel.kt
+++ app/src/main/java/com/bangkit/crabify/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.bangkit.crabify
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bangkit.crabify.domain.useCases.app_entry.StartCrabifyAppEntry
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,15 +11,23 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(private val startCrabifyAppEntry: StartCrabifyAppEntry) :
+    ViewModel() {
 
-    private val _isReady = MutableStateFlow(false)
-    val isReady = _isReady.asStateFlow()
+    private val _isCrabifyAppStarted = MutableStateFlow(true)
+    val isCrabifyAppStarted = _isCrabifyAppStarted.asStateFlow()
 
     init {
+        startAppEntry()
+    }
+
+    private fun startAppEntry() {
         viewModelScope.launch {
+            startCrabifyAppEntry().collect { isStarted ->
+                _isCrabifyAppStarted.value = isStarted
+            }
             delay(3000)
-            _isReady.value = true
+            _isCrabifyAppStarted.value = false
         }
     }
 }
---
+++ app/src/main/java/com/bangkit/crabify/presentation/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.crabify.presentation.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.crabify.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
